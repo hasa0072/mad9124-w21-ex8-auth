0c242ddac2ee8039306db079e1335bfb2901a59f
Sevda Hasanli <hasa0072@algonquinlive.com>
2021-03-15 22:42:47 -0400

    Login works

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
+const saltRounds = 14
 
 const schema = new mongoose.Schema({
   firstName: { type: String, trim: true, maxlength: 64, required: true },
@@ -6,6 +10,21 @@ const schema = new mongoose.Schema({
   email: { type: String, trim: true, unique: true, maxlength: 512, required: true },
   password: { type: String, trim: true, maxlength: 70, required: true }
 })
+
+schema.methods.generateAuthToken = function() {
+  const payload = {uid: this._id}
+  return jwt.sign(payload, 'superSecureSecret')
+}
+
+schema.statics.authenticate = async function (email, password) {
+  const user = await this.findOne({ email: email })
+  const badHash = `$2b$${saltRounds}$invalidusernameaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`
+  const hashedPassword = user ? user.password : badHash
+  const passwordDidMatch = await bcrypt.compare(password, hashedPassword)
+
+  return passwordDidMatch ? user : null
+  // remember if the email did not match, user === null
+}
 
 const Model = mongoose.model('User', schema) // factory function returns a class
 
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import bcrypt from 'bcrypt'
 import sanitizeBody from '../../middleware/sanitizeBody.js'
 import User from '../../models/User.js'
 const router = express.Router()
@@ -10,6 +9,21 @@ const saltRounds = 14
 router.post('/users', sanitizeBody, async (req, res) => {
   try {
     let newUser = new User(req.sanitizedBody)
+
+    // Check if user exists
+    const itExists = Boolean(await User.countDocuments({email: newUser.email}))
+    if (itExists) {
+      return res.status(400).send({
+        errors: [
+          {
+            status: '400',
+            title: 'Validation Error',
+            detail: `Email address '${newUser.email}' is already registered.`,
+            source: { pointer: '/data/attributes/email' }
+          }
+        ]
+      })
+    }
     newUser.password = await bcrypt.hash(newUser.password, saltRounds)
     await newUser.save()
     res.status(201).send({ data: newUser })
@@ -25,6 +39,26 @@ router.post('/users', sanitizeBody, async (req, res) => {
       ],
     })
   }
+})
+
+// Login a user and return an authentication token.
+router.post('/tokens', sanitizeBody, async (req, res) => {
+  const { email, password } = req.sanitizedBody
+  const user = await User.authenticate(email, password)
+  if (!user) {
+    return res.status(401).send({
+      errors: [
+        {
+          status: '401',
+          title: 'Incorrect username or password.',
+        },
+      ]
+    })
+  }
+
+  // if all is good, return a token
+  // if any condition failed, return an error message
+  res.status(201).send({ data: { token: user.generateAuthToken() } })
 })
 
 export default router
